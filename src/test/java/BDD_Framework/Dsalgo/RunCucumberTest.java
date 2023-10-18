30934fb51e1874a81b6ade74780d033ffd6c05ba
shreeja90 <shreejamenon09@gmail.com>
2023-10-18 15:47:50 -0400

    Framewrok Working Model with TestContext with pico Container

--- src/test/java/BDD_Framework/Dsalgo/RunCucumberTest.java
+++ src/test/java/BDD_Framework/Dsalgo/RunCucumberTest.java
@@ -7,8 +7,9 @@ import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 import org.junit.runner.RunWith;
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 
 
 @Suite
--- src/test/java/BDD_Framework/Dsalgo/StepDefinitions.java
+++ src/test/java/BDD_Framework/Dsalgo/StepDefinitions.java
@@ -1,38 +1,36 @@
 package BDD_Framework.Dsalgo;
 
-import org.openqa.selenium.WebDriver;
-
 import Pages.Homepage;
 import cucumber.TestContext;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import managers.PageObjectManager;
-import managers.WebDriverManager;
 
 
 public class StepDefinitions {
 	
-	WebDriver driver;
 	TestContext testContext;
 	Homepage homePage;
-	PageObjectManager pageObjectManager;	
-	WebDriverManager webDriverManager;
+	
+	public StepDefinitions(TestContext context) {
+		testContext = context;
+		homePage = testContext.getPageObjectManager().getHomePage();
+	}
 
 
     @Given("The user opens DS Algo portal link")
     public void openDsAlgoportal() {
-    	webDriverManager = new WebDriverManager();
+    	/*webDriverManager = new WebDriverManager();
 		driver = webDriverManager.getDriver();
 		pageObjectManager = new PageObjectManager(driver);
 		homePage = pageObjectManager.getHomePage();
+		testContext = context;*/
     	homePage.navigateTo_HomePage();
     }
 
 
     @Then("The user should land in DS Algo portal page")
     public void LandingonDsAlgoportal() {
-    	driver.quit();
     }
 
     @When("The user clicks the \"Get Started\" button")
